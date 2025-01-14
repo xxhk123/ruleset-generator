166fe0bda14f34b28b45efc32cacad01e243ac8d
xxhk123 <35906313+xxhk123@users.noreply.github.com>
2025-01-14 21:01:45 +0800

    Update generate_ruleset.sh

--- generate_ruleset.sh
+++ generate_ruleset.sh
@@ -4,7 +4,7 @@
 china_txt_url="https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/china.txt"
 curl -s -o china.txt "$china_txt_url"
 
-# 处理 china.txt 文件，生成 ruleset.json
+# 生成 ruleset.json 文件
 echo '{
   "version": 1,
   "rules": [
@@ -12,13 +12,16 @@ echo '{
       "ip_cidr": [' > ruleset.json
 
 # 将 china.txt 中的每行转换为符合 CIDR 格式的数组项
-awk '{ print "  \"" $0 "\"," }' china.txt >> ruleset.json
+awk '{ print "        \"" $0 "\"," }' china.txt >> ruleset.json
 
 # 删除最后一行多余的逗号，并关闭 JSON 数组和对象
 sed -i '$ s/,$//' ruleset.json
-echo ']
-}
-]
+
+# 结束 JSON 格式
+echo '
+      ]
+    }
+  ]
 }' >> ruleset.json
 
 echo "ruleset.json has been generated."
