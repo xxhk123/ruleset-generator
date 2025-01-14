e9c88736d6628a5782d3824c4b4c4d678749bcd3
xxhk123 <35906313+xxhk123@users.noreply.github.com>
2025-01-14 21:16:35 +0800

    Update generate_ruleset.sh

--- generate_ruleset.sh
+++ generate_ruleset.sh
@@ -11,13 +11,11 @@ echo '{
     {
       "ip_cidr": [' > ruleset.json
 
-# 将 china.txt 中的每行转换为符合 CIDR 格式的数组项
-# 使用 awk 去除多余的空格，并格式化成正确的 JSON 数组项
+# 将 china.txt 中的每行转换为 CIDR 格式并格式化
 awk '{gsub(/^[[:space:]]+|[[:space:]]+$/, ""); if(NF > 0) print "        \"" $0 "\""}' china.txt | sed '/^$/d' >> ruleset.json
 
-# 删除最后一行的逗号，关闭 JSON 数组和对象
-# 使用 head 命令删除最后一行的逗号
-head -n -1 ruleset.json > temp.json && mv temp.json ruleset.json
+# 删除最后一行的逗号
+sed -i '$ s/,$//' ruleset.json
 
 # 添加闭合的 JSON 语法
 echo '
@@ -34,10 +32,9 @@ echo "ruleset.json has been generated."
 git config --global user.email "action@github.com"
 git config --global user.name "GitHub Actions"
 
-# 添加所有修改的文件（包括新下载的 china.txt）
+# 添加修改的文件
 git add ruleset.json
 git add generate_ruleset.sh
-git add china.txt
 
 # 检查是否有修改，如果有修改则提交
 if git diff --staged --quiet; then
